9909aa8ba3bdbe5cb013ff005d6f91281a09cec6
Nathan Nichols <nwnichols02@gmail.com>
2022-02-11 12:25:41 -0700

    completed task 4, 7 passing of 11

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,22 +1,16 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
+  const topicsDiv = document.createElement("div");
+  topicsDiv.classList.add("topics");
 
-  const topicsDiv = document.createElement('div');
-  const javascriptDiv = document.createElement('div');
-  const bootstrapDiv = document.createElement('div');
-  const technologyDiv = document.createElement('div');
-
-  topicsDiv.appendChild(javascriptDiv);
-  topicsDiv.appendChild(bootstrapDiv);
-  topicsDiv.appendChild(technologyDiv);
-
-  topicsDiv.classList.add('topics');
-  javascriptDiv.classList.add('tab');
-  bootstrapDiv.classList.add('tab');
-  technologyDiv.classList.add('tab');
-
-  javascriptDiv.textContent = topics[0];
-  bootstrapDiv.textContent = topics[1];
-  technologyDiv.textContent = topics[2];
+  //This iterates through each topic argument that is passed into this function building each div.
+  topics.forEach((divElement) => {
+    const div = document.createElement("div");
+    topicsDiv.appendChild(div);
+    div.classList.add("tab");
+    div.textContent = divElement;
+  });
 
   return topicsDiv;
 
@@ -34,9 +28,22 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
+};
 
 const tabsAppender = (selector) => {
+  let appendElement = document.querySelector(selector);
+
+  axios
+    .get("http://localhost:5000/api/topics")
+    .then((resp) => {
+      console.log(resp.data.topics);
+
+      appendElement.appendChild(Tabs(resp.data.topics));
+
+      return appendElement;
+    })
+
+    .catch((err) => console.log(err));
   // TASK 4
   // ---------------------
   // Implement this function which takes a css selector as its only argument.
@@ -44,6 +51,6 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
-}
+};
 
-export { Tabs, tabsAppender }
+export { Tabs, tabsAppender };
