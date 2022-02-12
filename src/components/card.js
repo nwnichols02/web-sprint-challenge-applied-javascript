fa3a518c532157eacd612550470470bb8fa83cab
Nathan Nichols <nwnichols02@gmail.com>
2022-02-11 17:05:09 -0700

    passing 11/11 tests. finished proof reading

--- src/components/card.js
+++ src/components/card.js
@@ -1,56 +1,76 @@
-const Card = (article) => {
+import axios from "axios";
 
-  const card = document.createElement('div');
-  const headline = document.createElement('div');
-  const author = document.createElement('div');
-  const imgContainer = document.createElement('div');
-  const img = document.createElement('img');
-  const authorNameSpan = document.createElement('span');
+const Card = (article) => {
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const authorPhoto = document.createElement("img");
+  const authorNameSpan = document.createElement("span");
 
   card.appendChild(headline);
   card.appendChild(author);
   author.appendChild(imgContainer);
-  imgContainer.appendChild(img);
+  imgContainer.appendChild(authorPhoto);
   author.appendChild(authorNameSpan);
 
-  card.classList.add('card');
-  headline.classList.add('headline');
-  author.classList.add('author');
-  imgContainer.classList.add('img-container');
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
 
-  headline.textContent
-  img.src = authorPhoto;
-  authorNameSpan.textContent = `By ${authorName}`;
+  headline.textContent = article.headline;
+  authorNameSpan.textContent = article.authorName;
+  authorPhoto.src = article.authorPhoto;
 
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-}
+  card.addEventListener("click", (event) => {
+    console.log(article.headline);
+  });
+  return card;
+};
+
+// TASK 5
+// ---------------------
+// Implement this function, which should return the markup you see below.
+// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+//
+// <div class="card">
+//   <div class="headline">{ headline }</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={ authorPhoto }>
+//     </div>
+//     <span>By { authorName }</span>
+//   </div>
+// </div>
+//
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
+  axios
+    .get("http://localhost:5000/api/articles")
+    .then((resp) => {
+      const entry = document.querySelector(selector);
+      let topics = Object.keys(resp.data.articles);
+      for (let i = 0; i < topics.length; i++) {
+        resp.data.articles[topics[i]].forEach((articles) =>
+          entry.appendChild(Card(articles))
+        );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-export { Card, cardAppender }
+// TASK 6
+// ---------------------
+// Implement this function that takes a css selector as its only argument.
+// It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+// However, the articles do not come organized in a single, neat array. Inspect the response closely!
+// Create a card from each and every article object in the response, using the Card component.
+// Append each card to the element in the DOM that matches the selector passed to the function.
+
+export { Card, cardAppender };
--- src/components/header.js
+++ src/components/header.js
@@ -1,59 +1,50 @@
 import axios from "axios";
 
-// import { createBroadcastChannel } from "msw/lib/types/utils/createBroadcastChannel";
-console.log('hello world.')
-
 const Header = (title, date, temp) => {
-
-  const header = document.createElement('div');
-  const dateSpan = document.createElement('span');
-  const titleH1 = document.createElement('h1');
-  const tempSpan = document.createElement('span');
+  const header = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const titleH1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
 
   header.appendChild(dateSpan);
   header.appendChild(titleH1);
   header.appendChild(tempSpan);
 
-  header.classList.add('header');
-  dateSpan.classList.add('date');
-  tempSpan.classList.add('temp');
+  header.classList.add("header");
+  dateSpan.classList.add("date");
+  tempSpan.classList.add("temp");
 
   dateSpan.textContent = date;
   titleH1.textContent = title;
   tempSpan.textContent = temp;
-  // console.log(header);
+
   return header;
-
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
-}
-
-Header('lambda', 'date', 'temp')
+};
+// TASK 1
+// ---------------------
+// Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+//  <div class="header">
+//    <span class="date">{ date }</span>
+//    <h1>{ title }</h1>
+//    <span class="temp">{ temp }</span>
+//  </div>
+//
 
 const headerAppender = (selector) => {
   let appendElement = document.querySelector(selector);
 
-  appendElement.appendChild(Header('Lambda Times', 'January 6, 2021', '26°'));
+  appendElement.appendChild(Header("Lambda Times", "January 6, 2021", "26°"));
 
   return appendElement;
+};
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
+// It should create a header using the Header component above, passing arguments of your choosing.
+// It should append the header to the element in the DOM that matches the given selector.
+//
 
-  // TASK 2
-  // ---------------------
-  // X- Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
-}
-
-
-export { Header, headerAppender }
+export { Header, headerAppender };
